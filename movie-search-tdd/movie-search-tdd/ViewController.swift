ac15a34617935a030b3a7ba1c25a9b37533fa841
NullSleep <nullsleep@me.com>
2018-03-16 13:57:42 -0500

    First commit; project squeleton

---
+++ movie-search-tdd/movie-search-tdd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  movie-search-tdd
+//
+//  Created by Carlos Arenas on 3/16/18.
+//  Copyright © 2018 Carlos Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
