9943fae5d2e6aa0dbfef6483693428b80c30a370
NullSleep <nullsleep@me.com>
2018-03-16 15:56:14 -0500

    Carthage files and libraries.

--- movie-search-tdd/movie-search-tddTests/NetworkServicesTests.swift
+++ movie-search-tdd/movie-search-tddTests/NetworkServicesTests.swift
@@ -12,6 +12,9 @@ import XCTest
 /// Network services tess for the movie search API endpoints.
 class NetworkServicesTests: XCTestCase {
     
+    /// Singleton for handling NetworkServices requests
+    let networkServices = NetworkServices()
+    
     override func setUp() {
         super.setUp()
     }
@@ -21,12 +24,44 @@ class NetworkServicesTests: XCTestCase {
     }
     
     /**
-     This fucntion tests the API search endpoint
+     This fucntion tests the API search endpoint for a success answer disregarding the data
      */
-    func getSuccessfullSearchResultsForRandomMovieTest() {
-        let networkServices = NetworkServices()
-        let result = networkServices.searchTerm(term: "Batman", page: 1)
+    func getSearchResultsForRandomMovieTest() {
+        
         XCTAssertEqual(result, true)
+        
+        let e = expectation(description: "NetworkServices Search API")
+        
+//        XCTAssertNil(response.error, "Whoops, error \(response.error!.localizedDescription)")
+//        XCTAssertNotNil(response, "No response")
+//        XCTAssertEqual(response.response?.statusCode ?? 0, 200, "Status code not 200")
+        
+        networkServices.performSearch(for: "batman", page: 1) {
+            movieData, error in
+            
+            var jsonValue: JSON = [:] {
+                didSet {
+                    self.createMovies(json: jsonValue, page: self.currentPage)
+                }
+            }
+            
+            if movieData != nil {
+                self.searchActive = false;
+                
+                jsonValue = movieData!
+                
+                // self.storeMovies(json: movieData!)
+                
+                // Save the searched term
+                self.saveSearchTerm(term)
+                
+            } else if movieData == nil {
+                // jsonValue = self.retrieveStoredData() ?? [:]
+            }
+        }
+        
+        
+        waitForExpectations(timeout: 5.0, handler: nil)
     }
     
 }
